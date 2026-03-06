3e294038d8fddf9990350547b1acf6e831305d31
J.P.Y.K. Jayalath <168125065+Kalhara187@users.noreply.github.com>
2026-03-06 23:31:40 +0530

    Create FutureFeatures.jsx

---
+++ frontend/src/pages/FutureFeatures.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FutureFeaturesSection from '../components/FutureFeatures'
+
+const FutureFeatures = () => {
+  return (
+    <div className="pt-16">
+      <FutureFeaturesSection />
+    </div>
+  )
+}
+
+export default FutureFeatures
